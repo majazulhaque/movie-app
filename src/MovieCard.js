ba902ce1c39264449866a14aaa7dbf2a356ebba5
Majaz <majazulhaque123@gmail.com>
2023-05-22 19:14:36 +0530

    implementing state and fetch data from this.state

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 class MovieCart extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            title: "The Avengers",
+            plot:"Supernatural powers shown in this movie",
+            price: 199,
+            rating: 8.9,
+        }
+    }
     render (){
+        const {title,plot,price,rating} = this.state;
         return (
             <>
             <div className="main">
@@ -10,12 +20,12 @@ class MovieCart extends React.Component{
                         <img src="https://lumiere-a.akamaihd.net/v1/images/p_avengersendgame_19751_e14a0104.jpeg" alt="Poster" />
                     </div>
                     <div className="right">
-                        <div className="title">The Avengers</div>
-                        <div className="plot">Supernatural powers shown in the movie</div>
-                        <div className="price">Rs. 999</div>
+                        <div className="title">{title}</div>
+                        <div className="plot">{plot}</div>
+                        <div className="price">Rs. {price}</div>
 
                         <div className="footer">
-                            <div className="rating">8.9</div>
+                            <div className="rating">{rating}</div>
                             <div className="star-dis">
                                 <img className="str-btn" src="https://cdn-icons-png.flaticon.com/128/43/43625.png" alt="minus" />
                                 <img className="stars" src="https://cdn-icons-png.flaticon.com/128/1828/1828884.png" alt="star" />
